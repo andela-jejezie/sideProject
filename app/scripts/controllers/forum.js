5c0d214d46cb9c0758360be6a131eea0858b1ca3
ejezie <johnsonejezie@gmail.com>
2014-12-07 21:16:28 +0100

    deleted underscore

--- app/scripts/controllers/forum.js
+++ app/scripts/controllers/forum.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('forumCtrl', function ($scope, Auth, FIREBASE_URL, $routeParams, $rootScope, $firebase){
+app.controller('forumCtrl', function ($scope, Auth, FIREBASE_URL, $routeParams, $rootScope, $http, $firebase){
 	$scope.user = Auth.user;
 	var ref = new Firebase(FIREBASE_URL);
 	$scope.forumCatRef = $firebase(ref.child('forum_category')).$asObject();
@@ -10,10 +10,8 @@ app.controller('forumCtrl', function ($scope, Auth, FIREBASE_URL, $routeParams, 
 		var chatCollection = $firebase(ref.child('forum_chats')).$asObject();
 		var userRef = $firebase(ref.child('user'));
 		chatCollection.$loaded().then(function(data){
-			console.log(data);
 			data.forEach(function(value, key){
 				for(var k in value) {
-					console.log(value[k]);
 					var creatorDetails = $firebase(ref.child('user').child(value[k].creator)).$asObject();
 
 					creatorDetails.$loaded().then(function(userSnap){
@@ -21,11 +19,24 @@ app.controller('forumCtrl', function ($scope, Auth, FIREBASE_URL, $routeParams, 
 					$scope.details.push(object);
 				})
 				}
-				
-
 			})			
 		})
 	};
+
+	$scope.isSelected = function(value) {
+    	return $scope.selected === value;
+	}
+
+	$scope.getCategory = function(value) {
+	    return $http.get(ref + '/forum_chats/' + value + '.json'
+	    	).then(function(response){
+	    	var chatCollection = response.data;
+	    	console.log(chatCollection);
+	      // return response.data.results.map(function(item){
+	      //   return item.formatted_address;
+	      // });
+	    });
+	  };
 
 	var merge = function() {
     	var obj = {},
